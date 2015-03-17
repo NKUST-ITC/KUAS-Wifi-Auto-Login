ea140a9a5efde19bde74029287285837ac515387
HearSilent <hear.silent1995@gmail.com>
2015-03-17 16:56:01 +0800

    修正Notification縮圖錯誤, 修正震動權限

--- KUASWifiAutoLogin/app/src/main/java/tw/edu/kuas/wifiautologin/WifiAutoLogin.java
+++ KUASWifiAutoLogin/app/src/main/java/tw/edu/kuas/wifiautologin/WifiAutoLogin.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -270,10 +272,16 @@ public class WifiAutoLogin extends Activity {
                 LoginHandler.sendEmptyMessage(-1);
                 // Check if login finish
                 if (isConnect()) {
+                    final int notifyID = 1;
+                    final int requestCode = notifyID;
+                    final Intent intent = getIntent();
+                    final int flags = PendingIntent.FLAG_CANCEL_CURRENT;
+                    final PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), requestCode, intent, flags);
+
                     NotificationManager mNotificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-                    Notification notification=new Notification(R.drawable.ic_launcher, "高應無線通",  System.currentTimeMillis());
+                    Notification notification=new Notification(R.mipmap.ic_launcher, "高應無線通",  System.currentTimeMillis());
                     notification.defaults=Notification.DEFAULT_ALL;
-                    notification.setLatestEventInfo(WifiAutoLogin.this,"高應無線通", "Wi-Fi 登入成功！", null);
+                    notification.setLatestEventInfo(WifiAutoLogin.this,"高應無線通", "Wi-Fi 登入成功！", pendingIntent);
                     mNotificationManager.notify(1000, notification);
                     showToast(login_msg);
                 } else {
