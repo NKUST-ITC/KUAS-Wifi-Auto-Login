fa29c6a9d7a00045d8c2391fb751a9ff6881dff5
HearSilent <hear.silent1995@gmail.com>
2018-01-13 13:36:58 +0800

    Code tune

--- Android/app/src/main/java/tw/edu/kuas/wifiautologin/widget/WidgetConfigureActivity.java
+++ Android/app/src/main/java/tw/edu/kuas/wifiautologin/widget/WidgetConfigureActivity.java
@@ -24,98 +24,106 @@ import tw.edu.kuas.wifiautologin.libs.Memory;
 import tw.edu.kuas.wifiautologin.libs.Utils;
 
 public class WidgetConfigureActivity extends AppCompatActivity {
-    @BindView(R.id.button_save)Button mSaveButton;
-    @BindView(R.id.button_finish) Button mFinishButton;
-    @BindView(R.id.editText_user)EditText mUsernameEditText;
-    @BindView(R.id.editText_password) EditText mPasswordEditText;
-    @BindView(R.id.textInputLayout_user)TextInputLayout mUserNameTextInputLayout;
-    @BindView(R.id.textInputLayout_password) TextInputLayout mPasswordTextInputLayout;
-    private static Tracker mTracker;
 
-    private static int REQUEST_WRITE_SYSTEM_SETTINGS_LOGIN = 200;
+	@BindView(R.id.button_save) Button mSaveButton;
+	@BindView(R.id.button_finish) Button mFinishButton;
+	@BindView(R.id.editText_user) EditText mUsernameEditText;
+	@BindView(R.id.editText_password) EditText mPasswordEditText;
+	@BindView(R.id.textInputLayout_user) TextInputLayout mUserNameTextInputLayout;
+	@BindView(R.id.textInputLayout_password) TextInputLayout mPasswordTextInputLayout;
+	private static Tracker mTracker;
 
-    private int mAppWidgetId =AppWidgetManager.INVALID_APPWIDGET_ID;
+	private final static int REQUEST_WRITE_SYSTEM_SETTINGS_LOGIN = 200;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_widget_configure);
+	private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
-        // Set the result to CANCELED. This will cause the widget host to cancel
-        // out of the widget placement if the user presses the back button.
-        setResult(RESULT_CANCELED);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_widget_configure);
 
-        ButterKnife.bind(this);
+		// Set the result to CANCELED. This will cause the widget host to cancel
+		// out of the widget placement if the user presses the back button.
+		setResult(RESULT_CANCELED);
 
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        if (extras != null) {
-            mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-        }
+		ButterKnife.bind(this);
 
-        setUpViews();
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+			mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+					AppWidgetManager.INVALID_APPWIDGET_ID);
+		}
 
-    }
-    private void setUpViews() {
-        mUsernameEditText.setText(Memory.getString(this, Constant.MEMORY_KEY_USER, ""));
-        mPasswordEditText.setText(Memory.getString(this, Constant.MEMORY_KEY_PASSWORD, ""));
-        mPasswordEditText.setImeActionLabel(getText(R.string.ime_submit), KeyEvent.KEYCODE_ENTER);
-        mPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+		setUpViews();
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                mSaveButton.performClick();
-                return false;
-            }
-        });
-        mUserNameTextInputLayout.setHint(getString(R.string.id_hint));
-        mPasswordTextInputLayout.setHint(getString(R.string.password_hint));
-        initGA();
-    }
-    @OnClick(R.id.button_save)
-    public void save(){
-        mTracker.send(new HitBuilders.EventBuilder().setCategory("UX").setAction("Click")
-                .setLabel("Save").build());
-        String user = mUsernameEditText.getText().toString();
-        String pwd = mPasswordEditText.getText().toString();
-        //save user & password to SharedPreferences
-        Memory.setString(this, Constant.MEMORY_KEY_USER, user);
-        Memory.setString(this, Constant.MEMORY_KEY_PASSWORD, pwd);
+	}
 
-        Toast.makeText(this,R.string.save,Toast.LENGTH_SHORT).show();
+	private void setUpViews() {
+		mUsernameEditText.setText(Memory.getString(this, Constant.MEMORY_KEY_USER, ""));
+		mPasswordEditText.setText(Memory.getString(this, Constant.MEMORY_KEY_PASSWORD, ""));
+		mPasswordEditText.setImeActionLabel(getText(R.string.ime_submit), KeyEvent.KEYCODE_ENTER);
+		mPasswordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
-        checkGoogleBug();
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				mSaveButton.performClick();
+				return false;
+			}
+		});
+		mUserNameTextInputLayout.setHint(getString(R.string.id_hint));
+		mPasswordTextInputLayout.setHint(getString(R.string.password_hint));
+		initGA();
+	}
 
-    }
-    @OnClick(R.id.button_finish)
-    public void mFinish(){
-        mTracker.send(new HitBuilders.EventBuilder().setCategory("UX").setAction("Click")
-                .setLabel("Finish").build());
-        // Make sure we pass back the original appWidgetId
-        Intent resultValue = new Intent();
-        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-        setResult(RESULT_OK, resultValue);
-        save();
+	@OnClick(R.id.button_save)
+	public void save() {
+		mTracker.send(
+				new HitBuilders.EventBuilder().setCategory("UX").setAction("Click").setLabel("Save")
+						.build());
+		String user = mUsernameEditText.getText().toString();
+		String pwd = mPasswordEditText.getText().toString();
+		//save user & password to SharedPreferences
+		Memory.setString(this, Constant.MEMORY_KEY_USER, user);
+		Memory.setString(this, Constant.MEMORY_KEY_PASSWORD, pwd);
 
-        finish();
+		Toast.makeText(this, R.string.save, Toast.LENGTH_SHORT).show();
 
-    }
-    private void initGA() {
-        mTracker = ((SilentApplication) getApplication()).getDefaultTracker();
-        mTracker.setScreenName("Widget Configure Screen");
-    }
-    private void checkGoogleBug(){
+		checkGoogleBug();
 
-        if (Utils.checkGoogleBug()) {
-            if (!Utils.checkSystemWritePermission(this)) {
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
-                        .setAction("Denied").setLabel(Utils.getPhoneName()).build());
-                         Utils.showSystemWritePermissionDialog(this, REQUEST_WRITE_SYSTEM_SETTINGS_LOGIN);
-            } else {
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
-                        .setAction("Allowed").setLabel(Utils.getPhoneName()).build());
-            }
-        }
+	}
 
-    }
+	@OnClick(R.id.button_finish)
+	public void mFinish() {
+		mTracker.send(new HitBuilders.EventBuilder().setCategory("UX").setAction("Click")
+				.setLabel("Finish").build());
+		// Make sure we pass back the original appWidgetId
+		Intent resultValue = new Intent();
+		resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+		setResult(RESULT_OK, resultValue);
+		save();
+
+		finish();
+
+	}
+
+	private void initGA() {
+		mTracker = ((SilentApplication) getApplication()).getDefaultTracker();
+		mTracker.setScreenName("Widget Configure Screen");
+	}
+
+	private void checkGoogleBug() {
+
+		if (Utils.checkGoogleBug()) {
+			if (!Utils.checkSystemWritePermission(this)) {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
+						.setAction("Denied").setLabel(Utils.getPhoneName()).build());
+				Utils.showSystemWritePermissionDialog(this, REQUEST_WRITE_SYSTEM_SETTINGS_LOGIN);
+			} else {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
+						.setAction("Allowed").setLabel(Utils.getPhoneName()).build());
+			}
+		}
+
+	}
 }
--- Android/app/src/main/java/tw/edu/kuas/wifiautologin/widget/WidgetService.java
+++ Android/app/src/main/java/tw/edu/kuas/wifiautologin/widget/WidgetService.java
@@ -1,6 +1,5 @@
 package tw.edu.kuas.wifiautologin.widget;
 
-
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
@@ -24,140 +23,143 @@ import tw.edu.kuas.wifiautologin.models.UserModel;
 
 public class WidgetService extends Service {
 
-    private static Tracker mTracker;
+	private static Tracker mTracker;
 
-    public WidgetService() {
-    }
+	public WidgetService() {
+	}
 
-    @Override
-    public void onCreate() {
-        initGA();
-    }
+	@Override
+	public void onCreate() {
+		initGA();
+	}
 
-    private void initGA() {
-        mTracker = ((SilentApplication) getApplication()).getDefaultTracker();
-        mTracker.setScreenName("widget");
-    }
+	private void initGA() {
+		mTracker = ((SilentApplication) getApplication()).getDefaultTracker();
+		mTracker.setScreenName("widget");
+	}
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-            login();
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		login();
 
-        return START_NOT_STICKY;
-    }
+		return START_NOT_STICKY;
+	}
 
-    private void changeWidgetMessage(String message, int color) {
-        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.widget);
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-        ComponentName thisAppWidget = new ComponentName(getPackageName(), WidgetProvider.class.getName());
-        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
-        final int N = appWidgetIds.length;
-        remoteViews.setTextViewText(R.id.widget_message, message);
-        remoteViews.setTextColor(R.id.widget_message, color);
-        for (int i = 0; i < N; i++) {
-            int appWidgetId = appWidgetIds[i];
-            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-        }
+	private void changeWidgetMessage(String message, int color) {
+		RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.widget);
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+		ComponentName thisAppWidget =
+				new ComponentName(getPackageName(), WidgetProvider.class.getName());
+		int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+		final int N = appWidgetIds.length;
+		remoteViews.setTextViewText(R.id.widget_message, message);
+		remoteViews.setTextColor(R.id.widget_message, color);
+		for (int i = 0; i < N; i++) {
+			int appWidgetId = appWidgetIds[i];
+			appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+		}
 
-    }
+	}
 
-    @Override
-    public void onDestroy() {
-        Log.d("service", "onDestroy");
-    }
+	@Override
+	public void onDestroy() {
+		Log.d("service", "onDestroy");
+	}
 
-    public void login() {
-        String user = Memory.getString(this, Constant.MEMORY_KEY_USER, "");
-        String pwd = Memory.getString(this, Constant.MEMORY_KEY_PASSWORD, "");
-        UserModel model;
-        if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pwd)) {
-            model = Utils.tranUser(Constant.DEFAULT_GUEST_ACCOUNT, Constant.DEFAULT_GUEST_PWD);
-        } else {
-            model = Utils.tranUser(user, pwd);
-        }
+	public void login() {
+		String user = Memory.getString(this, Constant.MEMORY_KEY_USER, "");
+		String pwd = Memory.getString(this, Constant.MEMORY_KEY_PASSWORD, "");
+		UserModel model;
+		if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pwd)) {
+			model = Utils.tranUser(Constant.DEFAULT_GUEST_ACCOUNT, Constant.DEFAULT_GUEST_PWD);
+		} else {
+			model = Utils.tranUser(user, pwd);
+		}
 
-        String ssid = Utils.getCurrentSSID(this);
+		String ssid = Utils.getCurrentSSID(this);
 
-        if (Constant.EXPECTED_SSIDS.get(2).equals(ssid)) {
-            model.loginType = UserModel.LoginType.DORM;
-        }
+		if (Constant.EXPECTED_SSIDS.get(2).equals(ssid)) {
+			model.loginType = UserModel.LoginType.DORM;
+		}
 
-        LoginHelper.login(this, Utils.tranUser(user, pwd), new GeneralCallback() {
+		LoginHelper.login(this, Utils.tranUser(user, pwd), new GeneralCallback() {
 
-            @Override
-            public void onSuccess(final String message) {
-                mTracker.send(
-                        new HitBuilders.EventBuilder().setCategory("login").setAction("success")
-                                .setLabel(message).build());
-                changeWidgetMessage(message, getResources().getColor(R.color.md_green_400));
-                stopSelf();
-            }
+			@Override
+			public void onSuccess(final String message) {
+				mTracker.send(
+						new HitBuilders.EventBuilder().setCategory("login").setAction("success")
+								.setLabel(message).build());
+				changeWidgetMessage(message, getResources().getColor(R.color.md_green_400));
+				stopSelf();
+			}
 
-            @Override
-            public void onFail(final String reason) {
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("login").setAction("fail")
-                        .setLabel(reason).build());
-                changeWidgetMessage(reason, getResources().getColor(R.color.md_amber_800));
-                stopSelf();
-            }
+			@Override
+			public void onFail(final String reason) {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("login").setAction("fail")
+						.setLabel(reason).build());
+				changeWidgetMessage(reason, getResources().getColor(R.color.md_amber_800));
+				stopSelf();
+			}
 
-            @Override
-            public void onAlready() {
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("login").setAction("alreadyLogin").build());
-                changeWidgetMessage(getResources().getString(R.string.widget_already_connect), getResources().getColor(R.color.md_green_400));
-                stopSelf();
-            }
-        });
-    }
+			@Override
+			public void onAlready() {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("login")
+						.setAction("alreadyLogin").build());
+				changeWidgetMessage(getResources().getString(R.string.widget_already_connect),
+						getResources().getColor(R.color.md_green_400));
+				stopSelf();
+			}
+		});
+	}
 
-    public void logout() {
-        mTracker.send(new HitBuilders.EventBuilder().setCategory("UX").setAction("Click")
-                .setLabel("Logout").build());
-        if (Utils.checkGoogleBug()) {
-            if (!Utils.checkSystemWritePermission(this)) {
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
-                        .setAction("Denied").setLabel(Utils.getPhoneName()).build());
-                return;
-            } else {
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
-                        .setAction("Allowed").setLabel(Utils.getPhoneName()).build());
-            }
-        }
-        LoginHelper.logout(this, true, new GeneralCallback() {
+	public void logout() {
+		mTracker.send(new HitBuilders.EventBuilder().setCategory("UX").setAction("Click")
+				.setLabel("Logout").build());
+		if (Utils.checkGoogleBug()) {
+			if (!Utils.checkSystemWritePermission(this)) {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
+						.setAction("Denied").setLabel(Utils.getPhoneName()).build());
+				return;
+			} else {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("Write System Permission")
+						.setAction("Allowed").setLabel(Utils.getPhoneName()).build());
+			}
+		}
+		LoginHelper.logout(this, true, new GeneralCallback() {
 
-            @Override
-            public void onSuccess(final String message) {
+			@Override
+			public void onSuccess(final String message) {
 
-                mTracker.send(
-                        new HitBuilders.EventBuilder().setCategory("logout").setAction("success")
-                                .setLabel(message).build());
-                changeWidgetMessage(message, getResources().getColor(R.color.md_deep_orange_a400));
-                stopSelf();
-            }
+				mTracker.send(
+						new HitBuilders.EventBuilder().setCategory("logout").setAction("success")
+								.setLabel(message).build());
+				changeWidgetMessage(message, getResources().getColor(R.color.md_deep_orange_a400));
+				stopSelf();
+			}
 
-            @Override
-            public void onFail(final String reason) {
+			@Override
+			public void onFail(final String reason) {
 
-                mTracker.send(new HitBuilders.EventBuilder().setCategory("logout").setAction("fail")
-                        .setLabel(reason).build());
-                changeWidgetMessage(reason, getResources().getColor(R.color.md_deep_orange_a400));
-                stopSelf();
-            }
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("logout").setAction("fail")
+						.setLabel(reason).build());
+				changeWidgetMessage(reason, getResources().getColor(R.color.md_deep_orange_a400));
+				stopSelf();
+			}
 
-            @Override
-            public void onAlready() {
-            mTracker.send(new HitBuilders.EventBuilder().setCategory("logout")
-                        .setAction("alreadyLogout").build());
+			@Override
+			public void onAlready() {
+				mTracker.send(new HitBuilders.EventBuilder().setCategory("logout")
+						.setAction("alreadyLogout").build());
 
-                stopSelf();
-            }
-        });
-    }
+				stopSelf();
+			}
+		});
+	}
 
 }
